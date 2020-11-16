37084fbc8348fd9d55c1744ca33b517c68dcfda9
安田学 <info@manabuyasuda.com>
2020-11-16 16:35:24 +0900

    Add: 画像最適化処理を改善
    1. svgoの設定を追加
    2. 実行処理を自動化

--- scripts/imagemin.js
+++ scripts/imagemin.js
@@ -20,12 +20,36 @@ const minify = (input, destination) => {
     plugins: [
       imageminMozjpeg({ quality: 80 }),
       imageminPngquant({ quality: [0.6, 0.8] }),
-      imageminSvgo(),
+      imageminSvgo({
+        plugins: [
+          // viewBox属性を削除する（widthとheight属性がある場合）。
+          // 表示が崩れる原因になるので削除しない。
+          { removeViewBox: false },
+          // <metadata>を削除する。
+          // 追加したmetadataを削除する必要はない。
+          { removeMetadata: false },
+          // SVGの仕様に含まれていないタグや属性、id属性やversion属性を削除する。
+          // 追加した要素を削除する必要はない。
+          { removeUnknownsAndDefaults: false },
+          // コードが短くなる場合だけ<path>に変換する。
+          // アニメーションが動作しない可能性があるので変換しない。
+          { convertShapeToPath: false },
+          // 重複や不要な`<g>`タグを削除する。
+          // アニメーションが動作しない可能性があるので変換しない。
+          { collapseGroups: false },
+          // SVG内に<style>や<script>がなければidを削除する。
+          // idにアンカーが貼られていたら削除せずにid名を縮小する。
+          // id属性は動作の起点となることがあるため削除しない。
+          { cleanupIDs: false },
+        ],
+      }),
     ],
   }).then(() => {
-    console.log('Images optimized')
+    console.log(`Optimized image output to "${destination}"`)
   })
 }
 
-minify(paths.blog.input, paths.blog.destination)
-minify(paths.news.input, paths.news.destination)
+Object.keys(paths).forEach((path) => {
+  const key = paths[path]
+  minify(key.input, key.destination)
+})
